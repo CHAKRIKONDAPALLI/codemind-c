18fd000c3e9fb644693dc110e289834f0b027f96
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-01-23 23:49:30 +0530

    #include<stdio.h> int main() {     int a,b,i,c,k=0;     scanf("%d%d%d",&a,&b,&c);     for(i=a;i<=b;i++)      {          if(i%c==0)          k++;      }      printf("%d",k); }

---
+++ Count_Divisors.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c,k=0;
+    scanf("%d%d%d",&a,&b,&c);
+    for(i=a;i<=b;i++)
+     {
+         if(i%c==0)
+         k++;
+     }
+     printf("%d",k);
+}
