d49c27e97723a3d31bc2d8d6906025e79f0240c2
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-01-24 00:16:50 +0530

    #include<stdio.h> int main() {     int a=0,b=1,c,i,max,min,e,f,fib,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         c=a+b;         a=b;         b=c;         if(c<n)         {         min=c;         }     else     {         max=c;         break;     }     }     e=n-min;     f=max-n;     //printf("%d %d",max,min);     if(e==f)     {        printf("%d %d",min,max);     }     else     {     fib=(e<f)?min:max;    printf("%d",fib);     } }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,32 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int a=0,b=1,c,n,d,e,f,min,max,i,g;
+    int a=0,b=1,c,i,max,min,e,f,fib,n;
     scanf("%d",&n);
-    for(i=1;i>0;i++)
-{
-    c=a+b;
-    a=b;
-    b=c;
-    if(c<n)
+    for(i=1;i<=n;i++)
     {
+        c=a+b;
+        a=b;
+        b=c;
+        if(c<n)
+        {
         min=c;
-    }
+        }
     else
     {
         max=c;
         break;
     }
-}
-e=n-min;
-f=max-n;
-if(e==f)
-{
-    printf("%d %d",min,max);
-}
-else
-{
-    g=(e<f)?min:max;
-    printf("%d",g);
-}
+    }
+    e=n-min;
+    f=max-n;
+    //printf("%d %d",max,min);
+    if(e==f)
+    {
+       printf("%d %d",min,max);
+    }
+    else
+    {
+    fib=(e<f)?min:max;
+   printf("%d",fib);
+    }
 }
