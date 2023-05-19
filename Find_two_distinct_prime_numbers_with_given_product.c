9d0d22cb4cc6fe23ef5fb36d00fd8139935c0054
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-05-19 15:52:05 +0530

    #include<stdio.h> int prime(int num) {     int i,count=0;     for(i=1;i<=num;i++)     {         if(num%i==0)         {             count++;         }         if(count==2)         {             return 1;         }     }         return 0;
    }
    int main()
    {
        int n,i,j,s=0;
        scanf("%d",&n);
        for(i=1;i<=n;i++)
        {
            for(j=1;j<=n;j++)
            {
                if(i*j==n)
                {
                    if(prime(i)==1 && prime(j)==1)
                    {
                        printf("%d %d",i,j);
                        s=1;
                    }
                }
                if(s==1)
                {
                    break;
                }
            }
        }
        if(s==0)
        {
            printf("-1");
        }
    }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,45 +1,44 @@
 #include<stdio.h>
+int prime(int num)
+{
+    int i,count=0;
+    for(i=1;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            count++;
+        }
+        if(count==2)
+        {
+            return 1;
+        }
+    }
+        return 0;
+    
+}
 int main()
 {
-    int a,b,c=0,i,j,p=0,k,l,s;
-    scanf("%d",&a);
-    for(i=1;i<=a;i++)
+    int n,i,j,s=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-     c=0;
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            if(i%j==0)
+            if(i*j==n)
             {
-                c++;
+                if(prime(i)==1 && prime(j)==1)
+                {
+                    printf("%d %d",i,j);
+                    s=1;
+                }
+            }
+            if(s==1)
+            {
+                break;
             }
         }
-        if(c==2)
-        {
-          for(k=1;k<=a;k++)
-          {
-              s=0;
-              for(l=1;l<=k;l++)
-              {
-                  if(k%l==0)
-                  {
-                      s++;
-                  }
-              }
-              if(s==2)
-              {
-                  if(i*k==a)
-                  {
-                      printf("%d %d",i,k);
-                      p=1;
-                      break;
-                  }
-              }
-          }
-        }
-        if(p==1)
-        break;
     }
-    if(p==0)
+    if(s==0)
     {
         printf("-1");
     }
