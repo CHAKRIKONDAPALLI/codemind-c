3ac7a73d89cbe113714fbf39858c0fac9c82a256
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-05-19 16:11:36 +0530

    #include<stdio.h> int main() { 	int x,y,i,m,count=0; 	scanf("%d%d",&x,&y); 	for(i=1;i<=x*y;i++) 	{ 		m=x*i;     	if(m%y==0) 	    { 		    printf("%d",m); 		    break; 	    } 	}
    }

--- LCM.c
+++ LCM.c
@@ -1,25 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,x,y,i,p;
-    scanf("%d%d",&a,&b);
-    if(a>b)
-    {
-        x=a;
-        y=b;
-    }
-    else
-    {
-        x=b;
-        y=a;
-    }
-    for(i=1;i<x*b;i++)
-    {
-        p = x*i;
-        if(p%y==0)
-        {
-            printf("%d",p);
-            break;
-        }
-    }
+	int x,y,i,m,count=0;
+	scanf("%d%d",&x,&y);
+	for(i=1;i<=x*y;i++)
+	{
+		m=x*i;
+    	if(m%y==0)
+	    {
+		    printf("%d",m);
+		    break;
+	    }
+	}
+
+      	
+	
 }
