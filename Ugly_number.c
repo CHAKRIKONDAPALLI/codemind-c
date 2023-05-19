7e2e52b1749cf70c0d6dc144441e10fc4bc42d23
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-05-19 16:01:00 +0530

    #include<stdio.h> int main() {   int n,x=0;   scanf("%d",&n);        while (n != 1)        {             if (n % 5 == 0)             {                 n /= 5;             }             else if (n % 3 == 0)             {                 n /= 3;             }             else if (n % 2 == 0)             {                 n /= 2;             }             else             {                 printf("Not Ugly Number");                 x = 1;                 break;             }         } 		        if (x==0) 		        {                 printf("Ugly Number");                 } }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,12 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    int a;
-    scanf("%d",&a);
-    if(a==1 || a==2 || a==3|| a==4 || a==5 || a==6 ||a==8 || a==9 ||a==10 || a==12 || a==15)
-    {
-        printf("Ugly Number");
-    }
-    else
-    printf("Not Ugly Number");
+  int n,x=0;
+  scanf("%d",&n);
+       while (n != 1) 
+       {  
+            if (n % 5 == 0) 
+            {  
+                n /= 5;  
+            } 
+            else if (n % 3 == 0) 
+            {  
+                n /= 3;  
+            } 
+            else if (n % 2 == 0) 
+            {  
+                n /= 2;  
+            } 
+            else 
+            {  
+                printf("Not Ugly Number"); 
+                x = 1;  
+                break;  
+            }  
+        } 
+		        if (x==0)
+		        { 
+                printf("Ugly Number");
+                }
 }
