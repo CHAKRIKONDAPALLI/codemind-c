c7ce6a64d74089adb6441a66f02bfd3328478cf1
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-05-19 21:39:48 +0530

    #include<stdio.h> int main() {     int x,y,i;     scanf("%d%d",&x,&y);     if(x<y)     {         for(i=y;i>=1;i--)         {             if(x%i==0 && y%i==0)             {                 printf("%d",i);                 break;             }         }     } }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int i,a,b,gcd;
-    scanf("%d%d",&a,&b);
-    for(i=1;i<=a*b;i++)
+    int x,y,i;
+    scanf("%d%d",&x,&y);
+    if(x<y)
     {
-        if(a%i==0 && b%i==0)
-        gcd=i;
+        for(i=y;i>=1;i--)
+        {
+            if(x%i==0 && y%i==0)
+            {
+                printf("%d",i);
+                break;
+            }
+        }
     }
-    printf("%d",gcd);
 }
