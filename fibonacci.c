953ee90ddd564998665a0579824314228c62a0c0
CHAKRIKONDAPALLI <119840444+CHAKRIKONDAPALLI@users.noreply.github.com>
2023-05-19 15:49:56 +0530

    #include<stdio.h> int main() {     int n,a,b,sum,i;     scanf("%d",&n);     a=0;     b=1;     printf("%d %d ",a,b);     for(i=2;i<n;i++)     {         sum=a+b;         printf("%d ",sum);         a=b;         b=sum;     }
    }

--- fibonacci.c
+++ fibonacci.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int a=0,b=1,c,i,n;
+    int n,a,b,sum,i;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    a=0;
+    b=1;
+    printf("%d %d ",a,b);
+    for(i=2;i<n;i++)
     {
-        printf("%d ",a);
-        c=a+b;
+        sum=a+b;
+        printf("%d ",sum);
         a=b;
-        b=c;
+        b=sum;
     }
+    
 }
